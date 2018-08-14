c5e72a587d3c4821925be763facd4a749b254ee3
Sonu <you@example.com>
2018-08-14 11:44:35 +0530

    project upload

---
+++ src/main/java/com/hellocontroller.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hellocontroller {
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello world";
+	}
+}
